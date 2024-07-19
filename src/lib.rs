147451037275a9964d361f10c56ae9eafa20802a
Ezekiel Maroclo-Kemmerling@Mac <zkemmerling@gmail.com>
2024-07-19 09:02:23 -0400

    [IN PROGRESS] BVH implementation for faster intersection tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 mod aabb;
 mod brutus;
+mod coplanar;
 mod data;
 mod prelude;
 mod reader;
@@ -9,6 +10,9 @@ use std::collections::{HashMap, HashSet};
 
 use ordered_float::OrderedFloat as oF; // To hash floats
 
+// TODO: nalgebra and bvh have made the use of Vec3 and Point3 all fucky wucky
+//       it would be best to sort that out sooner rather than later
+
 // A proper mesh must not contain any edges which loop back on themselves (i->i)
 // To ensure uniqueness, we test for any duplicate vertices
 // If a vertex is unique, it will only appear once in the arrary such that a Set of the arrary will
@@ -16,6 +20,9 @@ use ordered_float::OrderedFloat as oF; // To hash floats
 pub fn validate_uniqueness(data: Data) -> bool {
     // construct a set of the vertices
     // Floats are not ordered and cannot be hashed without a wrapper
+    if data.triangles.len() == 0 {
+        return false;
+    }
     let hashit: HashSet<[oF<f32>; 3]> = data
         .vert()
         .iter()
@@ -78,25 +85,26 @@ pub fn boundedness(data: Data) -> bool {
 
 fn main() {}
 
+// TODO: clean up / move(?) these tests
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::reader::build_data;
+    use crate::reader::build_data_from_obj;
     // use std::vec;
 
     #[test]
     // Detect invalid vertices -> non-unique
     fn invalid_vertices() {
-        let data =
-            build_data("objs/cbl_test_dup.obj".to_string()).expect("failed to convert .obj file");
+        let data = build_data_from_obj("objs/cbl_test_dup.obj".to_string())
+            .expect("failed to convert .obj file");
         assert!(!validate_uniqueness(data))
     }
 
     #[test]
     // Test uniqueness of vertices
     fn valid_vertices() {
-        let data =
-            build_data("objs/cbl_test.obj".to_string()).expect("failed to convert .obj file");
+        let data = build_data_from_obj("objs/cbl_test.obj".to_string())
+            .expect("failed to convert .obj file");
         assert!(validate_uniqueness(data))
     }
 
@@ -104,21 +112,71 @@ mod tests {
     // Read a obj file and extract data into Data struct
     fn read_file() {
         let data =
-            build_data("objs/cbl_test.obj".to_string()).expect("failed to convert .obj file");
-        assert!(validate_uniqueness(data))
+            build_data_from_obj("objs/test1.obj".to_string()).expect("failed to convert .obj file");
+        println!("len: {}", data.triangles.len());
+        assert!(data.triangles.len() > 0);
     }
 
     #[test]
     // Test boundedness
     fn bound() {
-        let data =
-            build_data("objs/cbl_test.obj".to_string()).expect("failed to convert .obj file");
+        let data = build_data_from_obj("objs/cbl_test.obj".to_string())
+            .expect("failed to convert .obj file");
         assert!(boundedness(data))
     }
     #[test]
-    fn selfintersect() {
+    fn intersect() {
+        let data = build_data_from_obj("objs/test_intersect.obj".to_string())
+            .expect("failed to convert .obj file");
+        assert!(!intersection(data))
+    }
+    #[test]
+    fn intersect_coplanar_shared() {
+        let data = build_data_from_obj("objs/test_coplanar_square.obj".to_string())
+            .expect("failed to convert .obj file");
+        assert!(intersection(data))
+    }
+    #[test]
+    fn intersect_coplanar_disjoint() {
+        // funkier triangles
+        let data = build_data_from_obj("objs/test_coplanar_disjoint.obj".to_string())
+            .expect("failed to convert .obj file");
+        // normal coplanar triangles
+        let data2 = build_data_from_obj("objs/test_coplanar_disjoint2.obj".to_string())
+            .expect("failed to convert .obj file");
+        assert!(intersection(data) && intersection(data2))
+    }
+    #[test]
+    fn cube() {
         let data =
-            build_data("objs/cbl_test.obj".to_string()).expect("failed to convert .obj file");
+            build_data_from_obj("objs/cube.obj".to_string()).expect("failed to convert .obj file");
+        assert!(intersection(data))
+    }
+    #[test]
+    fn low_res_sphere() {
+        let data = build_data_from_obj("objs/low_sphere.obj".to_string())
+            .expect("failed to convert .obj file");
+        assert!(intersection(data))
+    }
+
+    #[test]
+    fn tetrahedron_selfintersect() {
+        let data = build_data_from_obj("objs/tetrahedrons_self_intersect.obj".to_string())
+            .expect("failed to convert .obj file");
+        assert!(!intersection(data))
+    }
+
+    #[test]
+    fn sphere_selfintersect() {
+        let data = build_data_from_obj("objs/sphere_selfintersect.obj".to_string())
+            .expect("failed to convert .obj file");
+        assert!(!intersection(data))
+    }
+    #[test]
+    #[ignore = "fails and we don't like that"]
+    fn high_polygon_no_intersect() {
+        let data = build_data_from_obj("objs/cbl_test.obj".to_string())
+            .expect("failed to convert .obj file");
         assert!(intersection(data))
     }
 }
--- src/prelude.rs
+++ src/prelude.rs
@@ -1,6 +1,7 @@
 // type Result<T> = Result<T, Box<dyn std::error::Error>>;
+use nalgebra::Point3;
 pub use nalgebra::Vector3 as Vec3;
 
 pub type Point = Vec3<f32>; // coordinates of a 3-d point
 pub type Index = Vec3<usize>; // a 3-vec of the indices pointing to the Points of a face
-pub type Face = Vec3<Point>; // a 3-vec of the points making up a triangular face
+pub type Face = Vec3<Point3<f32>>; // a 3-vec of the points making up a triangular face
